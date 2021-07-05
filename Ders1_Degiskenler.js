d85627756c525330846b263bb2b250a2bc7f9743
haydar <haydar@envepo.com>
2021-07-05 16:27:45 +0300

    degisken dersi bitti

---
+++ Ders1_Degiskenler.js
@@ -0,0 +1,25 @@
+
+// var let const farki
+
+var adim = "haydar";
+console.log("Benim Adim ==> "+adim)
+
+//  ES 6+ ile beraber let veya const'la degiskenler tanimlanir  let kullanilirsa degisken baska yerde degisime ugrar , artik var kullanilmamasi onerilir
+//  const kullanilirsa tanimlanan degisken sabittir birkez tanimlanir ve degismez
+
+const soyadim = "eren";
+console.log("Soyadim ==> "+soyadim)
+
+// sayi1  10 a esitlendi
+// daha sonra 20 ye esitlenip console.log  ile yazidirildi
+
+let sayi1=10;
+sayi1=20
+console.log("sayi1 degeri ==> "+sayi1)
+
+//camelCase olarak degisken isimlerini kullanacagiz , degisken isimleri sayi ile baslamaz,   (*arastiriniz:camelCase)
+
+const benimDogumYilim = 1993;
+console.log("Haydar Eren dogum yili ==> " + benimDogumYilim)
+
+
