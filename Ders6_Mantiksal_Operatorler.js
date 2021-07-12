0d268e215a4675cdcbfa4acc1402aed2c86718ad
haydar <haydar@envepo.com>
2021-07-12 11:16:14 +0300

    ders 6 olusturuldu

---
+++ Ders6_Mantiksal_Operatorler.js
@@ -0,0 +1,21 @@
+// ve veya tanimini yapacagiz
+// ve kullanildiginda
+// ve kullanildiginda iki kosulda saglanmalidir
+// veya kullanildiginda iki kosuldan birinin saglanmasi yeterli olur
+
+// &&  ve    ||  veya    olarak kullanilir.
+
+// ve oldugu iki kosuulunda karsilamasi gerekir ikiside saglarsa true donecektir
+
+let sayi1 = 10;
+let sayi2 = 20;
+
+// bu islem true dur
+console.log(sayi1 < 15 && sayi2 > 19)
+
+// bu islem false dir
+console.log(sayi1 < 15 && sayi2 < 19)
+
+// bu islem true alir cunlu sayi1 < 15 degeri dogru olmasi yeterli
+console.log(sayi1 < 15 || sayi2 < 19)
+
