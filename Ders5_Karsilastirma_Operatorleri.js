f36bd9dab2e80393475ca8ceb2d7e08aed930e90
haydar <haydar@envepo.com>
2021-07-12 10:55:58 +0300

    ders 5 bitti karsilastirma operatorleri

---
+++ Ders5_Karsilastirma_Operatorleri.js
@@ -0,0 +1,26 @@
+
+let sayi1 = 10
+let sayi2 = 20
+
+//sayi2 = sayi2 + sayi1
+sayi2 += sayi1
+console.log(sayi2)
+
+//sayi2 buyukmu sayi1 den diye soralim
+console.log(sayi2 > sayi1)
+
+//sayi2 kucuk ve esitmi diye soralim
+console.log(sayi2 <= sayi1)
+
+// sayi2 esit degil mi sayi1 e
+console.log("esit degil dogrumu ::: ",sayi1 != sayi2)
+
+// bun konu onemli lutfen bununla ilgili video izleyiniz
+const numberYas = 28;
+const stringYas = "28";
+
+console.log( "degerleri esit  ::: ", numberYas == stringYas); // ==
+console.log( "degeri ve veri tipi esit ::: ", numberYas === stringYas); // ===  false hayir esit degil
+console.log( "degeri ve veri tipi esit DEGIL::: ", numberYas !== stringYas); // !== sonuc true gelecek yani evet esit degil
+
+
